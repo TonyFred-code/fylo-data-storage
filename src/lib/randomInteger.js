d2c048dccb729ecd5997bb4fc9f45a48d715f3c9
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2026-04-03 14:13:34 +0100

    refactor: improve readability of arg normalization
    move randomInteger argument normalization to two separate line

--- src/lib/randomInteger.js
+++ src/lib/randomInteger.js
@@ -1,4 +1,5 @@
 export default function randomInteger(min, max) {
-  ((min = Math.floor(min)), (max = Math.ceil(max)));
+  min = Math.floor(min);
+  max = Math.ceil(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
